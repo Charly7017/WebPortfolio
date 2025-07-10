04234a6388fb0e9ad112b1ac5a3ee22e3b9e179e
Carlos Santamaria Solano <alexrace18@gmail.com>
2025-07-10 11:43:13 -0600

    Se quita el # por defecto en url anchors

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
+
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
 function toggleMenu() {
     const menu = document.querySelector('.header__menu');
     menu.classList.toggle('active');
 }
+
+
+
+
+
